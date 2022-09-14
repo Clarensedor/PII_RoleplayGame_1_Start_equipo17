78d3fd383c7170b0e29bc017b6058fec2cf519ec
juang0nz <101676463+juang0nz@users.noreply.github.com>
2022-09-13 21:19:46 -0300

    Clase Elfo comenzada

---
+++ src/Library/Elfo.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Program
+{
+    class Elfo
+    {
+        public string name;
+        private int vida = 100;
+        public int daño = 100;
+
+        private int defensa = 100;
+
+        public string Name { get => name; set => name = value; }
+        public int Daño { get => daño; set => daño = value; }
+        public int Defensa { get => defensa; set => defensa = value; }
+
+        //PLANTEO LOS METODOS PARA EVALUAR ENTRE TODOS EN CLASE
+        
+        public int RecibirDaño ()
+        {
+            return a;
+        }
+        
+        public int Curarse ()
+        {
+            return a;
+        }
+        
+        public int ObtenerDefensa ()
+        {
+            return a;
+        }
+        
+        public int ObtenerAtaque ()
+        {
+            return a;
+        }
+        
+        public int EquiparItem ()
+        {
+            return a;
+        }
+    }
+}
