8f26a8970012e614de1ca1e86d56d0af02035228
FacuPintos <facupintos@hotmail.com>
2022-09-17 04:08:50 -0300

    Se agrega clase Pocion

---
+++ src/Library/Items/Pocion.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace program {
+
+    public class Pocion {
+
+        public string name {get ; set}
+        public int value {get ; set}
+
+
+        public Pocion(string name, int value){
+
+            this.name = name;
+            this.value = value;
+        }        
+    }
+}
