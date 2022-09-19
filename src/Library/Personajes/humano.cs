49c602cd18cc2340fd9fcb2fd278e4ff326eda2d
FacuPintos <facupintos@hotmail.com>
2022-09-19 00:40:58 -0300

    Se corrije clase Humano

--- src/Library/Personajes/humano.cs
+++ src/Library/Personajes/humano.cs
@@ -4,27 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Program
 {
-   //Se declara la clase
-   public class Humano{
-
-      //Se definen los atributos/propiedades
+    public class Humano
+    {      
+    //Se definen los atributos/propiedades
       private string nombre;
       private int vida;
       private int danio;
       private int defensa;
+      private Espada espada;
+      private Escudo escudo;
 
       public  int vidaTotal;
 
       //se define el constructor
-      public Humano(string nombre, int vida, int danio, int defensa)
+      public Humano(string nombre, int vida, int danio, int defensa, Espada espada, Escudo escudo)
       {
          this.Nombre = nombre;
          this.Vida = vida;
          vidaTotal = vida;
          this.Danio = danio;
          this.Defensa = defensa;
+         this.Espada = espada;
+         this.Escudo = escudo;
       }
 
       //definimos getters y setters
@@ -34,20 +38,39 @@ namespace Program
 
       public int Danio{get{return this.danio;}set{this.danio = value;}}
       public int Defensa{get{return this.defensa;}set{this.defensa = value;}}
-
+      public Espada Espada{get{return this.espada;}set{this.espada = value;}}
+      public Escudo Escudo{get{return this.escudo;}set{this.escudo = value;}}
 
       //metodos/comportamientos
       
       //Recibe daño
       public void RecibirDanio(int dañoRecibido)
       {
+
          this.vida = this.vida - (dañoRecibido - this.defensa);
+         if(this.vida < 0){
+          this.vida = 0;
+         }
+      
       }
 
       //Vuelve a su vida inicial
       public void Curarse()
       {
+
           this.vida = vidaTotal;
+
+      }
+
+      public void EquiparEspada(){
+
+        this.Danio += this.Espada.Ataque;
+
+      }
+      
+      public void EquiparEscudo(){
+
+        this.Danio += this.Escudo.Defensa;
       }
 
     }
